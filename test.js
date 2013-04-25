1ac66ef270c6cb943f931ae0ce81a0408d69311d
TJ Holowaychuk <tj@vision-media.ca>
2013-04-24 17:45:23 -0700

    remove old test thing

--- test.js
+++ test.js
@@ -1,41 +1,8 @@
-var 
-	graphicsMagick = require('./build/Release/GraphicsMagick'),
-	fs = require('fs');
+var gm = require('./build/Release/GraphicsMagick');
+var fs = require('fs');
 
-var inputFile = "test.jpg";
-var outputFile = "output.jpg";
+var buf = fs.readFileSync('test.jpg');
+var img = gm.image(buf);
 
-console.log("Opening "+inputFile+"...")
-fs.readFile(inputFile, function (err, data) {
-	
-	if (err) 
-		throw err;
-	
-	console.log("Reading image...");
-	var inputImage = graphicsMagick.image(data);
-	
-	console.log("Image has following properties: ");
-	console.log("Width: "+inputImage.width);
-	console.log("Height: "+inputImage.height);
-		
-	console.log("Creating output image...");
-	var outputImage = inputImage
-		.crop(Math.floor(inputImage.width/2 - 200/2), Math.floor(inputImage.height/2 - 200/2), 200, 200)
-		.resize(100, 100);
-	
-	console.log("Creating write stream for "+outputFile+"...");
-	var writeStream = fs.createWriteStream(outputFile);
-	
-	writeStream.on("end", function(){
-		console.log("Done.");
-	});
-	
-	console.log("Writing new image data...");
-	writeStream.write(outputImage.buffer);
-	console.log("Closing stream...");
-	writeStream.end();
-	
-	
-	
-	
-});
+img.format('JPEG');
+console.log(img.buffer);
